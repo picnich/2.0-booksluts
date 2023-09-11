33f1e4fe1fdaf0ce3568bb13869cb6bc5e028a35
Nicholas Pereira <nicholaspereira@Nichs-Lap-2.local>
2023-09-11 13:38:36 -0400

    added cms.js and updated index.html

---
+++ src/admin/js/cms.js
@@ -0,0 +1,8 @@
+import {
+    ColorControl,
+    ColorPreview,
+} from "netlify-cms-widget-colorpicker";
+
+CMS.registerWidget("color", ColorControl, ColorPreview);
+// CMS.registerPreviewStyle("../styles/bulma.css");
+// CMS.registerPreviewStyle("../styles/styles.css");
